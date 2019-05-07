47f7fc184c0c11ef7d5d6ee600ffad0228ceb4e8
Radu <raducandea99@gmail.com>
2019-05-07 12:09:50 +0200

    Finished king & maybe almost the catalog
    King:
    - inplemented the methods required for the king to get an amount am values from the treasure, then to print it to the catalog and then to sleep for 15 seconds (the party hard time)
    - the value is between 100 and 200
    
    Catalog:
    -implemented the required things for the class to be a thread safe singleton
    -added methods to print out:
    ~the party canceling
    ~throwing a party with a certain amount
    ~showing how many valuables are in the treasure room
    ~showing if valuables were added to the treasure room by the tax collector

--- Assignment3/src/King.java
+++ Assignment3/src/King.java
@@ -1,15 +1,27 @@
+import static java.lang.Thread.sleep;
+
 public class King implements Runnable {
 
     private TreasureRoom treasure;
+    private Catalog catalog;
 
     public King(TreasureRoom treasure) {
         this.treasure = treasure;
+        this.catalog = catalog.getInstance();
     }
 
     @Override
     public void run() {
-        treasure.getValue(randomValue(100,200));
-        
+        while (true) {
+            int value = randomValue(100, 200);
+            treasure.getValue(value);
+            catalog.throwParty(value);
+            try {
+                sleep(15000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public int randomValue(int min, int max) {
