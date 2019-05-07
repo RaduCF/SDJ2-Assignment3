d8075dc41c43dec4198437abb6e67848c8bb37f4
Radu <raducandea99@gmail.com>
2019-05-07 11:03:48 +0200

    Class Diagram

--- Assignment3/src/King.java
+++ Assignment3/src/King.java
@@ -1,3 +1,18 @@
-public class King {
-    
+public class King implements Runnable {
+
+    private TreasureRoom treasure;
+
+    public King(TreasureRoom treasure) {
+        this.treasure = treasure;
+    }
+
+    @Override
+    public void run() {
+        treasure.getValue(randomValue(100,200));
+        
+    }
+
+    public int randomValue(int min, int max) {
+        return (int) Math.floor(Math.random() * (max - min)) + min;
+    }
 }
